30413ff6ddbd86b4f8b9ac8c2b41a595584505f3
TakahashiIkki <new_ittyandesu@outlook.jp>
2018-04-27 00:04:11 +0900

    キャッシュ周りを実装

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,92 @@
+var CACHE_NAME = 'my-okamoba-cache-v1';
+var urlsToCache = [
+    '/',
+    '/index.html',
+    '/?from=hom',
+    '/icon.png',
+    '/service-worker.js'
+];
+
 /** service worker.js の各イベント */
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function(event) {
+    // インストール時処理： ファイルをキャッシュさせるような処理をここに記述する。
     console.log('[ServiceWorker] Install');
+
+    // どれか１つでもダウンロードに失敗したら ServiceWorkerのinstall処理は失敗となる.
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function(event) {
+    // Service Workerの更新
     console.log('[ServiceWorker] Activate');
+
+    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+        return Promise.all(
+            cacheNames.map(function(cacheName) {
+                // Service Worker内のキャッシュをループ: ホワイトリスト内に無いキャッシュは削除.
+                if (cacheWhitelist.indexOf(cacheName) === -1) {
+                    return caches.delete(cacheName);
+                }
+            })
+        );
+        })
+    );
 });
 
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) {
+    // Service Worker がインストールされた状態で、
+    // 他のページヘ移動したりページを更新したりすると、Service Worker は fetch イベントを受け取ります。
+
+    event.respondWith(
+        caches.match(event.request)
+          .then(function(response) {
+            if (response) {
+                // キャッシュがあったのでそのレスポンスを返す
+                return response;
+            }
+            // // ↓ 下のように記述する事で、キャッシュが無ければネットワークリクエストを投げるようになる。
+            // return fetch(event.request);
+
+            // ↓ 下のように書く事でリクエストを再キャッシュさせるようになる。
+
+            // 重要：リクエストを clone する。リクエストは Stream なので
+            // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+            // 必要なので、リクエストは clone しないといけない
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+                function(response) {
+                  // レスポンスが正しいかをチェック
+                  if(!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                  }
+      
+                  // 重要：レスポンスを clone する。レスポンスは Stream で
+                  // ブラウザ用とキャッシュ用の2回必要。なので clone して
+                  // 2つの Stream があるようにする
+                  var responseToCache = response.clone();
+      
+                  caches.open(CACHE_NAME)
+                    .then(function(cache) {
+                        // cloneしなかった リクエストをCache用に使用する。
+                        // Cache用にCloneしたレスポンスをキャッシュする。
+                        cache.put(event.request, responseToCache);
+                    });
+      
+                    // キャッシュしなかった方のレスポンスはブラウザに渡す.
+                    return response;
+                }
+              );
+          }
+        )
+      );
+});
